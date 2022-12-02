a6cba903e4276e2d4db10258492b611dbaebea2f
Luis Ignacio Chapa <luisignaciochapa@gmail.com>
2022-12-01 21:53:44 -0300

    tercera

--- eleccion.js
+++ eleccion.js
@@ -1,5 +1,6 @@
 // Array que contiene el Menú
 const Menu = [];
+
 // Clase constructora de objetos
 class Producto {
   constructor(nombre, precio) {
@@ -7,27 +8,25 @@ class Producto {
     this.precio = precio;
   }
 }
+
 // Objetos y método .Push
-const Plato1 = new Producto(`Milanesa con puré`, 5);
+const Plato1 = new Producto(`Milanesa con puré`, 500);
 Menu.push(Plato1);
-const Plato2 = new Producto(`Bife con ensalada`, 6);
+const Plato2 = new Producto(`Bife con ensalada`, 600);
 Menu.push(Plato2);
-const Plato3 = new Producto(`Fideos con salsa`, 4);
+const Plato3 = new Producto(`Fideos con salsa`, 400);
 Menu.push(Plato3);
-const Bebida1 = new Producto(`Agua`, 3);
+const Bebida1 = new Producto(`Agua`, 100);
 Menu.push(Bebida1);
-const Bebida2 = new Producto(`Jugo`, 4);
+const Bebida2 = new Producto(`Jugo`, 150);
 Menu.push(Bebida2);
-const Bebida3 = new Producto(`Vino`, 3);
+const Bebida3 = new Producto(`Vino`, 200);
 Menu.push(Bebida3);
 
-
 // buscar los elementos del DOM
-
 const selectElem = document.getElementById('lista')
 const botonAgregar = document.getElementById('agregar')
-const botonFinzalizar = document.getElementById('finalizar')
-
+const botonFinalizar = document.getElementById('finalizar')
 
 Menu.forEach(prod=>{
    const optionProd =  document.createElement('option')
@@ -38,18 +37,17 @@ Menu.forEach(prod=>{
 
 // carrito
 const carrito = []
-//console.log(productosArray)
 botonAgregar.onclick = () =>{
     const nomProd = selectElem.value
     const prodSelec = Menu.find(prod=>prod.nombre === nomProd)
     carrito.push(prodSelec)
-    console.log(carrito)
+    swal(`¡Producto agregado!`);
 }
 
-botonFinzalizar.onclick = () =>{
+botonFinalizar.onclick = () =>{
     let totalCompra = 0
     carrito.forEach(prod=>{
         totalCompra = totalCompra + parseInt(prod.precio)
     })
-    alert(`El total a pagar es ${totalCompra}`)
+    swal(`¡Buen trabajo!`, `El total a pagar es $${totalCompra}`);
 }
